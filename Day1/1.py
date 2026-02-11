404034ca76409a71bd77b1c0b5c52fc761638266
HarikaMurali <harikamurali2005@gmail.com>
2026-02-11 12:46:36 +0530

    Python Intro

---
+++ Day1/1.py
@@ -0,0 +1,5 @@
+print("Harika is motivated to learn Python programming language.");
+print("A","B","C",sep="*");
+print("A","B","C",sep="%");
+print("Hello","Welcome","All",sep="-",end="Thnx\n");
+print("Java");
